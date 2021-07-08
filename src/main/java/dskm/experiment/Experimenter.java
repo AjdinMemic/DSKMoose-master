286f939731723dad98d76aec8c4334f8af09042d
AjdinMemic <ajdinmemic99@outlook.com>
2021-07-08 09:32:51 +0200

    Experimenter class refactored, all log checking methods moved to logChecker class

--- src/main/java/dskm/experiment/Experimenter.java
+++ src/main/java/dskm/experiment/Experimenter.java
@@ -15,7 +15,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Experimenter {
     private Constellation testConstellation = Constellation.FITTS_CURSORSIZE_1;
-    private String participantID = retNumbLog(); // Participant's ID (Pa1, Pa2, ...)
+    private String participantID = logChecker.retNumbLog(); // Participant's ID (Pa1, Pa2, ...)
 
     private static Experimenter self = null; // for singleton
 
@@ -464,63 +464,5 @@ public class Experimenter {
 
     public String getTestType(){
         return testConstellation.getTestType();
-    }
-
-    public static String retNumbLog() throws IOException {
-
-        if(logList().length==1) {
-            checkIflogEmpty();
-        }
-
-        String retVal = "";
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("..\\DSKMoose-master\\testlogs\\number.txt"));
-            String strLine;
-            //Read File Line By Line
-            while ((strLine = br.readLine()) != null) {
-                // Print the content on the console
-                retVal += strLine;
-            }
-
-            int append = Integer.parseInt(retVal);
-            append++;
-            String appendStr= String.valueOf(append);
-
-            FileWriter writer = new FileWriter("..\\DSKMoose-master\\testlogs\\number.txt");
-            BufferedWriter buffer = new BufferedWriter(writer);
-
-            buffer.write(appendStr);
-            buffer.close();
-
-            //Close the input stream
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return retVal;
-    }
-
-    public static String[] logList(){
-        //Creating a File object for directory
-        File directoryPath = new File("..\\DSKMoose-master\\testlogs");
-        //List of all files and directories
-        String contents[] = directoryPath.list();
-
-        return contents;
-    }
-
-    public static void checkIflogEmpty() throws IOException {
-        String[] lengthOfLogsFold=logList();
-        FileWriter fileWriter = new FileWriter("..\\DSKMoose-master\\testlogs\\number.txt");
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        String numb="0";
-        if(lengthOfLogsFold.length==1){
-
-            bufferedWriter.write(numb);
-        }
-        bufferedWriter.close();
-        fileWriter.close();
     }
 }
---
+++ src/main/java/dskm/experiment/logChecker.java
@@ -0,0 +1,65 @@
+package dskm.experiment;
+
+import java.io.*;
+
+public class logChecker {
+
+    // Increments the number.txt file for every start of the application
+    public static String retNumbLog() throws IOException {
+
+        if (logList().length == 1) {
+            checkIflogEmpty();
+        }
+
+        String retVal = "";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("..\\DSKMoose-master\\testlogs\\number.txt"));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                // Print the content on the console
+                retVal += strLine;
+            }
+
+            int append = Integer.parseInt(retVal);
+            append++;
+            String appendStr = String.valueOf(append);
+
+            FileWriter writer = new FileWriter("..\\DSKMoose-master\\testlogs\\number.txt");
+            BufferedWriter buffer = new BufferedWriter(writer);
+
+            buffer.write(appendStr);
+            buffer.close();
+
+            //Close the input stream
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //if(retVal.equals("0")){retVal="1";}
+        return retVal;
+    }
+
+    public static String[] logList() {
+        //Creating a File object for directory
+        File directoryPath = new File("..\\DSKMoose-master\\testlogs");
+        //List of all files and directories
+        String contents[] = directoryPath.list();
+
+        return contents;
+    }
+
+    public static void checkIflogEmpty() throws IOException {
+        String[] lengthOfLogsFold = logList();
+        FileWriter fileWriter = new FileWriter("..\\DSKMoose-master\\testlogs\\number.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        String numb = "1";
+        if (lengthOfLogsFold.length == 1) {
+            bufferedWriter.write(numb);
+        }
+        bufferedWriter.close();
+        fileWriter.close();
+    }
+}
